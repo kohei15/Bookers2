d970fa51939761d54317a126638ff137f2829006
木下晃兵 <kinoshitakohei@MacBook-Air-2.local>
2019-01-13 20:42:42 +0900

    first commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,46 @@
+class BooksController < ApplicationController
+  def show
+  	@book = Book.find(params[:id])
+  end
+
+  def top
+  end
+
+  def index
+  	@books = Book.all
+  	@book = Book.new
+  end
+
+  def create
+  	book = Book.new(book_params)
+  	if book.save
+  		flash[:notice] = "Book was successfully updated."
+  		redirect_to book_path(book.id)
+  	end
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def update
+	  book = Book.find(params[:id])
+	  book.update(book_params)
+	  redirect_to books_path
+  end
+
+  def destroy
+        book = Book.find(params[:id])
+        book.destroy
+        redirect_to books_path
+    end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+end
